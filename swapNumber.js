ad920bbf70f685c8ae44441b6f4ea5af2da5e6e8
saadjaved10 <125725266+saadjaved10@users.noreply.github.com>
2023-03-07 03:13:59 +0500

    Add files via upload

---
+++ swapNumber.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 7;
+let x;
+console.log(`value before swapping of a= ${a}`);
+console.log(`value before swapping of b= ${b}`);
+
+x = a;
+a = b;
+b = x;
+console.log(`value after swapping of a= ${a}`);
+console.log(`value after swapping of b= ${b}`);
